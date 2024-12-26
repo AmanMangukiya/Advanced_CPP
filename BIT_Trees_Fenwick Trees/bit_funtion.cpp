b08980ecb0c9cf549d97e0bef49668cc38390a2b
AmanMangukiya <153352716+AmanMangukiya@users.noreply.github.com>
2024-12-26 23:36:33 +0530

    Update bit_funtion.cpp

--- BIT_Trees_Fenwick Trees/bit_funtion.cpp
+++ BIT_Trees_Fenwick Trees/bit_funtion.cpp
@@ -9,6 +9,25 @@ int bit[N];
 // bit[i] stores the sum of after removing the right most bit fromt the i,let's suppose it's become j  
 // then bit[i] stores the sum  [j+1 to i]   1 base indexing
 // removing right most bit form the i ==> i-(i&-i);
+
+
+// suppose updete option found add at index i value x
+// but bacause of our bit tree deffination as describe above so add value at each posion 
+//so which get by adding right most bit to it's number
+
+// suppose update(5,x) then 5 => 101
+/* added bit[5]+=x, then   101 
+                          +001
+			  -----
+                           110 =>6
+
+      add bit[6]+=x, and son on till less then equal size of array or N
+
+      becuse 6=> 110
+      6&(-6) =>4 so it store te value form [5 to 6] 
+
+*/
+
 void update(int i, int x){ 
 	for(; i < N; i += (i&-i))             // (x&-x)  gives us right most but
     bit[i] += x;                                     /*   x= 12 ->1100 
